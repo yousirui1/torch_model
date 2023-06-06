fce7e75e2365b19fbf46a6681039f1a12cc89ac8
yousirui1 <yousirui1@163.com>
2023-06-06 10:12:20 +0800

    Update mobilenet.py
    mobilenet add activation

--- mobilenet.py
+++ mobilenet.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision.models import mobilenet_v2
 from torchinfo import summary
+import torch.nn.functional as F
 
 class MobileNetV2(nn.Module):
-    def __init__(self, input_dim, label_dim=527, pretrain=True):
+    def __init__(self, input_dim, label_dim=527, pretrain=True, activation = 'sigmoid'):
         super(MobileNetV2, self).__init__()
 
         self.input_dim = input_dim
@@ -13,6 +14,7 @@ class MobileNetV2(nn.Module):
 
         self.model.features[0][0] = torch.nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
         self.model.classifier = torch.nn.Linear(in_features=1280, out_features=label_dim, bias=True)
+        self.activation = activation
 
     def forward(self, x):
         # expect input x = (batch_size, time_frame_num, frequency_bins), e.g., (12, 1024, 128)
@@ -22,8 +24,10 @@ class MobileNetV2(nn.Module):
             x = x.view(self.input_dim[0], 1, self.input_dim[1], self.input_dim[2])
         #x = x.unsqueeze(1)
         x = x.transpose(2, 3)
-
-        out = torch.sigmoid(self.model(x))
+        if self.activation == 'sigmoid':
+            out = torch.sigmoid(self.model(x))
+        elif self.activation == 'softmax':
+            out = F.softmax(self.model(x), dim=1)
         return out
 
 if __name__ ==  '__main__':
